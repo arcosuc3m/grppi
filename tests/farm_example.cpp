706019b42e4029f62ea09c7d20abd48b6da76dab
Andres <andrsanc@inf.uc3m.es>
2017-06-05 12:00:18 +0200

    Revert farm_example

--- tests/farm_example.cpp
+++ tests/farm_example.cpp
@@ -89,7 +89,7 @@ void farm_example1() {
             for(int j = 0; j< v.size() ; j++) {
                 acumm += v[j];
             }
-						file << acumm;
+            file << acumm;
         }
     );
 }
