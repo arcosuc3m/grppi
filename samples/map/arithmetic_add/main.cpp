c72f2dad4e4894c9b70aa000087bedb3a727c458
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2017-06-08 15:23:12 +0200

    Updated example with dynamic policy

--- samples/map/arithmetic_add/main.cpp
+++ samples/map/arithmetic_add/main.cpp
@@ -23,57 +23,170 @@
 #include <chrono>
 #include <string>
 #include <numeric>
+#include <stdexcept>
+
+// Experimental C++
+#include <experimental/any>
 
 #include "map.h"
 
-
-void map_example1(int n, auto &p) {
-
-    using namespace std;
-    
-    std::vector<int> in(n);
-    std::iota(in.begin(), in.end(), 0);
-    std::vector<int> out(n);
-    
-    grppi::map(p, in.begin(), in.end(), out.begin(),
-         [](int i) { return i*2; } );
-
-    std::copy(begin(out), end(out), ostream_iterator<int>(cout, " "));
-    std::cout << std::endl;
-
+template <typename E>
+constexpr bool is_execution_policy() {
+  return std::is_same<E, grppi::sequential_execution>::value 
+      || std::is_same<E, grppi::parallel_execution_thr>::value
+      || std::is_same<E, grppi::parallel_execution_tbb>::value
+      || std::is_same<E, grppi::parallel_execution_omp>::value
+  ;
 }
+
+template <typename E>
+using requires_execution_policy =
+  std::enable_if_t<is_execution_policy<E>(), int>;
+
+class dynamic_execution;
+
+template <typename E>
+dynamic_execution make_dynamic_execution();
+
+class dynamic_execution {
+public:
+
+  /// Create empty dynamic execution.
+  dynamic_execution() :
+    policy_{},
+    policy_type_{nullptr}
+  {}
+
+  /// Determine if there is a policy.
+  bool has_execution() const { return policy_.get() != nullptr; }
+
+  const type_info & type() const noexcept { 
+    assert(has_execution());
+    return *policy_type_; 
+  }
+
+  template <typename E>
+  E * execution_ptr() {
+    assert(has_execution());
+    return (*policy_type_ != typeid(E))?nullptr:static_cast<E*>(policy_.get());
+  }
+
+private:
+  /// Pointer to dynamically allocated execution policy.
+  std::shared_ptr<void> policy_;
+
+  /// Type id of the current execution policy;
+  const type_info * policy_type_;
+
+private:
+
+  /// Create from other static policy with no arguments.
+  template <typename E>
+  friend dynamic_execution make_dynamic_execution() {
+    dynamic_execution e;
+    e.policy_ = make_shared<E>();
+    e.policy_type_ = &typeid(E);
+    return e;
+  }
+
+};
+
+template <typename I1, typename I2, typename F>
+void mymap_helper(dynamic_execution & e , I1 i1, I1 f1, I2 i2, F && f) {
+}
+
+template <typename E, typename ... O, typename I1, typename I2, typename F>
+void mymap_helper(dynamic_execution & e , I1 i1, I1 f1, I2 i2, F && f) {
+  if (typeid(E) == e.type()) {
+    grppi::map(*e.execution_ptr<E>(), i1, f1, i2, f);
+  }
+  else
+    mymap_helper<O...>(e, i1, f1, i2, f);
+}
+
+template <typename I1, typename I2, typename F>
+void mymap(dynamic_execution & e, I1 i1, I1 f1, I2 i2, F && f) {
+  mymap_helper<
+    grppi::sequential_execution,
+    grppi::parallel_execution_thr,
+    grppi::parallel_execution_omp,
+    grppi::parallel_execution_tbb
+  >(e, i1, f1, i2, f);
+}
+
+
+// EXAMPLE STARTS HERE
+
+dynamic_execution execution_mode(const std::string & opt) {
+  if ("seq" == opt) 
+    return make_dynamic_execution<grppi::sequential_execution>();
+  if ("thr" == opt) 
+    return make_dynamic_execution<grppi::parallel_execution_thr>();
+  if ("omp" == opt) 
+    return make_dynamic_execution<grppi::parallel_execution_omp>();
+  if ("tbb" == opt) 
+    return make_dynamic_execution<grppi::parallel_execution_tbb>();
+  return dynamic_execution{};
+}
+
+template <typename F, typename ...Args>
+bool run_test(const string & mode, F && f, Args && ... args) {
+  auto e = execution_mode(mode);
+  if (e.has_execution()) {
+    f(e, args...);
+    return true;
+  }
+  return false;
+}
+
+
+void test_map(dynamic_execution & e, int n) {
+  using namespace std;
+
+  vector<int> in(n);
+  iota(in.begin(), in.end(), 0);
+
+  vector<int> out(n);
+
+  mymap(e, begin(in), end(in), begin(out),
+    [](int i) { return i*2; });
+
+  copy(begin(out), end(out), ostream_iterator<int>(cout, " "));
+  cout << endl;
+}
+
+void print_message(const std::string & prog, const std::string & msg) {
+  using namespace std;
+  cerr << msg << endl;
+  cerr << "Usage: " << prog << " size mode" << endl;
+  cerr << "  size: Integer value with problem size" << endl;
+  cerr << "  mode:" << endl;
+  cerr << "    seq -> Sequential execution" << endl;
+  cerr << "    thr -> ISO Threads backend" << endl;
+  cerr << "    tbb -> Intel TBB backend" << endl;
+  cerr << "    omp -> OpenMP backend" << endl;
+}
+
 
 int main(int argc, char **argv) {
     
-    using namespace std;
+  using namespace std;
 
-    if(argc < 3){
-        std::cerr << "Invalid number of arguments. Usage: ./binary <problem_size> <SEQ|THR|TBB|OMP>" << std::endl;
-        return -1;
-    }
+  if(argc < 3){
+    print_message(argv[0], "Invalid number of arguments.");
+    return -1;
+  }
 
-    int n = stoi(argv[1]);
-    if(n <= 0){
-       std::cerr << "Invalid problem size. Use a positive integer number." << std::endl;
-        return -1;
-    }
+  int n = stoi(argv[1]);
+  if(n <= 0){
+    print_message(argv[0], "Invalid problem size. Use a positive number.");
+    return -1;
+  }
 
-    if(!strcmp("SEQ", argv[2])){
-        grppi::sequential_execution p{};
-        map_example1(n,p);
-    }else if (!strcmp("THR", argv[2])){
-        grppi::parallel_execution_thr p{};
-        map_example1(n,p);
-    }else if (!strcmp("TBB", argv[2])){
-        grppi::parallel_execution_tbb p{};
-        map_example1(n,p);
-    }else if (!strcmp("OMP", argv[2])){
-        grppi::parallel_execution_omp p{};
-        map_example1(n,p);
-    }else{
-        std::cerr << "Invalid policy. Usage: ./binary <problem_size> <SEQ|THR|TBB|OMP>" << std::endl;
-        return -1;
-    }
+  if (!run_test(argv[2], test_map, n)) {
+    print_message(argv[0], "Invalid policy.");
+    return -1;
+  }
 
     return 0;
 }
