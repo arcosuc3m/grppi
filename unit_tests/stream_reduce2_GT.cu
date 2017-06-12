18ecd01b7fb08c6801f338745b45b24647028eb0
Andres <andrsanc@inf.uc3m.es>
2017-06-12 09:18:51 +0200

    Update all mentions in code of version, from v0.1 to v0.2

--- unit_tests/stream_reduce2_GT.cu
+++ unit_tests/stream_reduce2_GT.cu
@@ -1,5 +1,5 @@
 /**
-* @version		GrPPI v0.1
+* @version		GrPPI v0.2
 * @copyright		Copyright (C) 2017 Universidad Carlos III de Madrid. All rights reserved.
 * @license		GNU/GPL, see LICENSE.txt
 * This program is free software: you can redistribute it and/or modify
