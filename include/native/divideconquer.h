354450f5ad8280854085ad348df78ba7677348c9
Drioastorga <drio@pa.uc3m.es>
2017-07-07 10:06:21 +0200

    Introduced files with the new names for divide and conquer

---
+++ include/native/divideconquer.h
@@ -0,0 +1,157 @@
+/**
+* @version		GrPPI v0.2
+* @copyright		Copyright (C) 2017 Universidad Carlos III de Madrid. All rights reserved.
+* @license		GNU/GPL, see LICENSE.txt
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You have received a copy of the GNU General Public License in LICENSE.txt
+* also available in <http://www.gnu.org/licenses/gpl.html>.
+*
+* See COPYRIGHT.txt for copyright notices and details.
+*/
+
+#ifndef GRPPI_DIVIDECONQUER_THR_H
+#define GRPPI_DIVIDECONQUER_THR_H
+
+#include <thread>
+#include <atomic>
+
+namespace grppi{
+template <typename Input, typename DivFunc, typename Operation, typename MergeFunc>
+typename std::result_of<Operation(Input)>::type internal_divide_conquer(parallel_execution_native &p, Input &problem,
+                                        DivFunc &&divide, Operation &&op, MergeFunc &&merge,
+                                        std::atomic<int> &num_threads) {
+  // Sequential execution fo internal implementation
+  using Output = typename std::result_of<Operation(Input)>::type;
+  sequential_execution seq;
+  Output out;
+   if(num_threads.load()>0){
+      auto subproblems = divide(problem);
+
+      if(subproblems.size()>1){
+        std::vector<Output> partials(subproblems.size()-1);
+//        num_threads -= subproblems.size();
+        int division = 0;
+        std::vector<std::thread> tasks;
+        auto i = subproblems.begin();
+        for(i = subproblems.begin()+1; i != subproblems.end() && num_threads.load()>0 ; i++, division++){
+            //THREAD
+            tasks.push_back(
+               std::thread(
+                   [&](auto i,int division){
+                    // Register the thread in the execution model
+                    p.register_thread(); 
+
+                    partials[division] = internal_divide_conquer(p, *i, std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge), num_threads);
+
+                    // Deregister the thread in the execution model
+                    p.deregister_thread();
+                  },
+                   i, division
+               )
+            );
+
+            num_threads--;
+            //END TRHEAD
+        }
+
+        for(i; i != subproblems.end(); i++){
+              partials[division] = divide_conquer(seq,*i, std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge));
+        }
+          //Main thread works on the first subproblem.
+
+        out = internal_divide_conquer(p, *subproblems.begin(), std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge), num_threads);
+          //JOIN
+        for(int i=0; i< tasks.size(); i++){
+          tasks[i].join();
+        }
+
+        for(int i = 0; i<partials.size();i++){ // MarcoA - this is moved to the user code
+           merge(partials[i], out);
+        }
+      }else{
+        out = op(problem);
+      }
+    }else{
+        return divide_conquer(seq, problem, std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge));
+    }
+    return out;
+}
+
+
+
+
+
+
+template <typename Input, typename DivFunc, typename Operation, typename MergeFunc>
+typename std::result_of<Operation(Input)>::type divide_conquer(parallel_execution_native& p, Input & problem,
+            DivFunc && divide, Operation && op, MergeFunc && merge) {
+    using Output = typename std::result_of<Operation(Input)>::type;
+    Output out;
+    // Sequential execution fo internal implementation
+    sequential_execution seq;
+    
+    std::atomic<int> num_threads (p.num_threads-1);
+    
+    if(num_threads.load()>0){
+      auto subproblems = divide(problem);
+
+      if(subproblems.size()>1){
+        std::vector<Output> partials(subproblems.size()-1);
+	    int division = 0;
+	    std::vector<std::thread> tasks;
+        auto i = subproblems.begin();
+        for(i = subproblems.begin()+1; i != subproblems.end() && num_threads.load()>0; i++, division++){
+            //THREAD
+	        tasks.push_back(
+		       std::thread(
+                   [&](auto i,int division){ 
+                    // Register the thread in the execution model
+                    p.register_thread();
+
+                    partials[division] = internal_divide_conquer(p, *i, std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge), num_threads);
+                   
+                    // Deregister the thread in the execution model
+                    p.deregister_thread();
+                  },
+                   i, division
+		       )
+	        );
+            num_threads --;
+            //END TRHEAD
+        }
+        for(i; i != subproblems.end(); i++){
+              partials[division] = divide_conquer(seq,*i, std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge));
+        }
+        
+        //Main thread works on the first subproblem.
+        out = internal_divide_conquer(p, *subproblems.begin(), std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge), num_threads);
+       
+
+          //JOIN
+        for(int i=0; i< tasks.size(); i++){
+          tasks[i].join();
+        }
+
+        for(int i = 0; i<partials.size();i++){ // MarcoA - this is moved to the user code
+           merge(partials[i], out);
+        }
+       }else{
+        out = op(problem);
+      }
+    }else{
+        return divide_conquer(seq, problem, std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge));
+    }
+    return out;
+}
+
+}
+#endif
---
+++ include/seq/divideconquer.h
@@ -0,0 +1,53 @@
+/**
+* @version		GrPPI v0.2
+* @copyright		Copyright (C) 2017 Universidad Carlos III de Madrid. All rights reserved.
+* @license		GNU/GPL, see LICENSE.txt
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You have received a copy of the GNU General Public License in LICENSE.txt
+* also available in <http://www.gnu.org/licenses/gpl.html>.
+*
+* See COPYRIGHT.txt for copyright notices and details.
+*/
+
+#ifndef GRPPI_DIVIDECONQUER_SEQ_H
+#define GRPPI_DIVIDECONQUER_SEQ_H
+namespace grppi{
+
+template <typename Input, typename DivFunc, typename Operation, typename MergeFunc>
+typename std::result_of<Operation(Input)>::type divide_conquer(sequential_execution &s, Input &problem, DivFunc &&divide,
+                               Operation &&op, MergeFunc &&merge) {
+     
+    using Output = typename std::result_of<Operation(Input)>::type;
+    auto subproblems = divide(problem);
+    Output out;
+    if(subproblems.size()>1){
+        std::vector<Output> partials(subproblems.size());
+	int division = 0;
+        for(auto i = subproblems.begin(); i != subproblems.end(); i++, division++){
+            //THREAD
+            partials[division] = divide_conquer(s, *i, std::forward<DivFunc>(divide), std::forward<Operation>(op), std::forward<MergeFunc>(merge) );
+            //END THREAD
+        }
+        out = partials[0] ;
+        //JOIN
+        for(int i = 1; i<partials.size();i++){
+              merge(partials[i], out);
+        }
+    }else{
+
+        out = op(problem);
+    }
+    return out;
+}
+
+}
+#endif
