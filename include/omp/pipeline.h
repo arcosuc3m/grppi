948e2d73375c2e7826542950603ecf49eac11dad
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2017-07-11 02:05:37 +0200

    Documented and edited code for pipeline OpenMP backend

--- include/omp/pipeline.h
+++ include/omp/pipeline.h
@@ -30,258 +30,333 @@
 namespace grppi{
 
 //Last stage
-template <typename Stream, typename Stage>
-void stages( parallel_execution_omp &p, Stream& st, Stage && s ){
+template <typename InQueue, typename Consumer>
+void pipeline_impl(parallel_execution_omp & ex, InQueue & input_queue, 
+                   Consumer && consume_op)
+{
+  using namespace std;
+  using input_type = typename InQueue::value_type;
 
-    //Start task
-    typename Stream::value_type item;
-    std::vector<typename Stream::value_type> elements;
+  if (ex.ordering){
+    vector<input_type> elements;
     long current = 0;
-    if(p.ordering){
-      item = st.pop( );
-      while( item.first ) {
-        if(current == item.second){
-           s( item.first.value() );
-           current ++;
-        }else{
-           elements.push_back(item);
-        }
-        for(auto it = elements.begin(); it != elements.end(); it++){
-           if((*it).second == current) {
-              s((*it).first.value());
+    auto item = input_queue.pop( );
+    while (item.first) {
+      if (current == item.second) {
+        consume_op( item.first.value() );
+        current ++;
+      } 
+      else {
+        elements.push_back(item);
+      }
+      for (auto it=elements.begin(); it!=elements.end(); it++) {
+        if (it->second == current) {
+          consume_op(*it->first);
               elements.erase(it);
               current++;
               break;
            }
         }
-       item = st.pop( );
+       item = input_queue.pop( );
       }
       while(elements.size()>0){
         for(auto it = elements.begin(); it != elements.end(); it++){
-          if((*it).second == current) {
-            s((*it).first.value());
+          if(it->second == current) {
+            consume_op(*it->first);
             elements.erase(it);
             current++;
             break;
           }
         }
       }
-    }else{
-      item = st.pop( );
-      while( item.first ) {
-        s( item.first.value() );
-        item = st.pop( );
+    }
+    else {
+      auto item = input_queue.pop();
+      while (item.first) {
+        consume_op(*item.first);
+        item = input_queue.pop();
      }
    }
    //End task
 }
 
-template <typename Operation, typename Stream,typename... Stages>
-void stages( parallel_execution_omp &p, Stream& st, filter_info<parallel_execution_omp, Operation> & se, Stages && ... sgs ) {
-  stages(p,st,std::forward<filter_info<parallel_execution_omp, Operation> &&>( se), std::forward<Stages>( sgs )...) ;
+template <typename Transformer, typename InQueue, typename... MoreTransformers>
+void pipeline_impl(parallel_execution_omp & ex, InQueue& input_queue, 
+                   filter_info<parallel_execution_omp,Transformer> & filter_obj, 
+                   MoreTransformers && ... more_transform_ops) 
+{
+  pipeline_impl(ex,input_queue,
+      std::forward<filter_info<parallel_execution_omp, Transformer>>(filter_obj), 
+      std::forward<MoreTransformers>(more_transform_ops)...) ;
 }
 
-template <typename Operation, typename Stream,typename... Stages>
- void stages( parallel_execution_omp &p, Stream& st, filter_info<parallel_execution_omp, Operation> && se, Stages && ... sgs ) {
-    if(p.ordering){
-       mpmc_queue< typename Stream::value_type > q(p.queue_size,p.lockfree);
+template <typename Transformer, typename InQueue,
+          typename... MoreTransformers>
+void pipeline_impl_ordered(parallel_execution_omp & ex, 
+                           InQueue & input_queue, 
+                           filter_info<parallel_execution_omp,Transformer> && filter_obj, 
+                           MoreTransformers && ... more_transform_ops)
+{
+  using namespace std;
+  using input_type = typename InQueue::value_type;
+  using input_value_type = typename input_type::first_type;
+  mpmc_queue<input_type> tmp_queue{ex.queue_size, ex.lockfree};
 
-       std::atomic<int> nend ( 0 );
-       for( int th = 0; th < se.exectype.num_threads; th++){
-           #pragma omp task shared(q,se,st,nend)
-           {
-                 typename Stream::value_type item;
-                 item = st.pop( ) ;
-                 while( item.first ) {
-                     if( se.task(item.first.value()) )
-                        q.push( item );
-                     else{
-                        q.push( std::make_pair( typename Stream::value_type::first_type()  ,item.second) );
-                     }
-                     item = st.pop();
-                 }
-                 nend++;
-                 if(nend == se.exectype.num_threads){
-                    q.push( std::make_pair(typename Stream::value_type::first_type(), -1) );
-                 }else{
-                    st.push(item);
-                 }
-           }
-       }
-       mpmc_queue< typename Stream::value_type > qOut(p.queue_size,p.lockfree);
-       #pragma omp task shared (qOut,q)
-       {
-          typename Stream::value_type item;
-          std::vector<typename Stream::value_type> elements;
-          int current = 0;
-          long order = 0;
-          item = q.pop( ) ;
-          while(1){
-             if(!item.first && item.second == -1){
-                 break;
-             }
-             if(item.second == current){
-                if(item.first){
-                   qOut.push(std::make_pair(item.first,order));
-                   order++;
-                }
-                current++;
-             }else{
-                elements.push_back(item);
-             }
-             for(auto it = elements.begin(); it < elements.end(); it++){
-                if((*it).second == current){
-                    if((*it).first){
-                        qOut.push(std::make_pair((*it).first,order));
-                        order++;
-                    }
-                    elements.erase(it);
-                    current++;
-                    break;
-                }
-             }
-             item=q.pop();
-          }
-          while(elements.size()>0){
-            for(auto it = elements.begin(); it < elements.end(); it++){
-              if((*it).second == current){
-                  if((*it).first){
-                     qOut.push(std::make_pair((*it).first,order));
-                     order++;
-                  }
-                  elements.erase(it);
-                  current++;
-                  break;
-              }
-            }
-          }
-          qOut.push(item);
-       }
-       stages(p, qOut, std::forward<Stages>(sgs) ... );
-       #pragma omp taskwait
-      }else{
-       mpmc_queue< typename Stream::value_type > q(p.queue_size,p.lockfree);
-
-       std::atomic<int> nend ( 0 );
-       for( int th = 0; th < se.exectype.num_threads; th++){
-             #pragma omp task shared(q,se,st,nend)
-             {
-                 typename Stream::value_type item;
-                 item = st.pop( ) ;
-                 while( item.first ) {
-                     if( se.task(item.first.value()) )
-                        q.push( item );
-//                     else{
-//                        q.push( std::make_pair( typename Stream::value_type::first_type()  ,item.second) );
-//                     } 
-                      item = st.pop();
-                 }
-                 nend++;
-                 if(nend == se.exectype.num_threads){
-                    q.push( std::make_pair(typename Stream::value_type::first_type(), -1) );
-                 }else{
-                    st.push(item);
-                 }
-
-          }
-       }
-       stages(p, q, std::forward<Stages>(sgs) ... );
-       #pragma omp taskwait
-    }
-}
-
-
-template <typename Operation, typename Stream,typename... Stages>
- void stages( parallel_execution_omp &p, Stream& st, farm_info<parallel_execution_omp, Operation> & se, Stages && ... sgs ) {
- stages(p,st, std::forward< farm_info<parallel_execution_omp, Operation> && >(se), std::forward<Stages>( sgs )...) ;
-}
-
-template <typename Operation, typename Stream,typename... Stages>
- void stages( parallel_execution_omp &p, Stream& st, farm_info<parallel_execution_omp, Operation> && se, Stages && ... sgs ) {
-  
- 
-    mpmc_queue< std::pair < std::experimental::optional < typename std::result_of< Operation(typename Stream::value_type::first_type::value_type) >::type >, long > > q(p.queue_size,p.lockfree);
-    std::atomic<int> nend ( 0 );
-    for( int th = 0; th < se.exectype.num_threads; th++){
-      #pragma omp task shared(nend,q,se,st)
-      {
-         auto item = st.pop();
-         while( item.first ) {
-         auto out = std::experimental::optional< typename std::result_of< Operation(typename Stream::value_type::first_type::value_type) >::type >( se.task(item.first.value()) );
-
-          q.push( std::make_pair(out,item.second)) ;
-          item = st.pop( );
+  atomic<int> nend{0}; // TODO: Find better name?
+  for(int th = 0; th<filter_obj.exectype.num_threads; th++) {
+    #pragma omp task shared(tmp_queue,filter_obj,input_queue,nend)
+    {
+      auto item{input_queue.pop()};
+      while (item.first) {
+        if(filter_obj.task(*item.first)) {
+          tmp_queue.push(item);
         }
-        st.push(item);
-        nend++;
-        if(nend == se.exectype.num_threads)
-          q.push(std::make_pair(std::experimental::optional< typename std::result_of< Operation(typename Stream::value_type::first_type::value_type) >::type >(), -1));
-      }              
+        else {
+          tmp_queue.push(make_pair(input_value_type{} ,item.second));
+        }
+        item = input_queue.pop();
+      }
+      nend++;
+      if (nend==filter_obj.exectype.num_threads) {
+        tmp_queue.push (make_pair(input_value_type{}, -1));
+      }
+      else {
+        input_queue.push(item);
+      }
     }
-    stages(p, q, std::forward<Stages>(sgs) ... );
-    #pragma omp taskwait
+  }
+
+  mpmc_queue<input_type> output_queue{ex.queue_size, ex.lockfree};
+  #pragma omp task shared (output_queue,tmp_queue)
+  {
+    vector<input_type> elements;
+    int current = 0;
+    long order = 0;
+    auto item = tmp_queue.pop();
+    for (;;) {
+      if (!item.first && item.second == -1) break;
+      if (item.second == current) {
+        if (item.first) {
+          output_queue.push(make_pair(item.first, order++));
+        }
+        current++;
+      }
+      else {
+        elements.push_back(item);
+      }
+      for (auto it=elements.begin(); it<elements.end(); it++) {
+        if ((*it).second==current) {
+          if((*it).first){
+            output_queue.push(make_pair((*it).first,order++));
+          }
+          elements.erase(it);
+          current++;
+          break;
+        }
+      }
+      item = tmp_queue.pop();
+    }
+    while (elements.size()>0) {
+      for (auto it=elements.begin(); it<elements.end(); it++) {
+        if ((*it).second == current) {
+          if((*it).first) {
+            output_queue.push(make_pair((*it).first,order++));
+          }
+          elements.erase(it);
+          current++;
+          break;
+        }
+      }
+    }
+    output_queue.push(item);
+  }
+  pipeline_impl(ex, output_queue, 
+      forward<MoreTransformers>(more_transform_ops)...);
+  #pragma omp taskwait
+}
+
+template <typename Transformer, typename InQueue,typename... MoreTransformers>
+void pipeline_impl_unordered(parallel_execution_omp & ex, InQueue & input_queue, 
+                             filter_info<parallel_execution_omp, Transformer> && farm_obj, 
+                             MoreTransformers && ... more_transform_ops)
+{
+  using input_type = typename InQueue::value_type;
+  using input_value_type = typename input_type::first_type;
+  mpmc_queue<input_type> output_queue{ex.queue_size, ex.lockfree};
+
+  std::atomic<int> nend{0}; //TODO: Better naming?
+  for (int th=0; th<farm_obj.exectype.num_threads; th++) {
+    #pragma omp task shared(output_queue,farm_obj,input_queue,nend)
+    {
+      auto item = input_queue.pop( ) ;
+      while (item.first) {
+        if (farm_obj.task(item.first.value())) {
+          output_queue.push(item);
+        }
+        item = input_queue.pop();
+      }
+      nend++;
+      if (nend==farm_obj.exectype.num_threads) {
+        output_queue.push(make_pair(input_value_type{}, -1));
+      }
+      else {
+        input_queue.push(item);
+      }
+    }
+  }
+  pipeline_impl(ex, output_queue, 
+      std::forward<MoreTransformers>(more_transform_ops)...);
+  #pragma omp taskwait
+}
+
+template <typename Transformer, typename InQueue,typename... MoreTransformers>
+void pipeline_impl(parallel_execution_omp & ex, InQueue & input_queue, 
+                   filter_info<parallel_execution_omp, Transformer> && filter_obj, 
+                   MoreTransformers && ... more_transform_ops) 
+{
+  if (ex.ordering) {
+    pipeline_impl_ordered(ex, input_queue,
+        std::forward<filter_info<parallel_execution_omp, Transformer>>(filter_obj),
+        std::forward<MoreTransformers>(more_transform_ops)...);
+  }
+  else {
+    pipeline_impl_unordered(ex, input_queue,
+        std::forward<filter_info<parallel_execution_omp, Transformer>>(filter_obj),
+        std::forward<MoreTransformers>(more_transform_ops)...);
+  }
 }
 
 
+template <typename Transformer, typename InQueue,typename... MoreTransformers>
+void pipeline_impl(parallel_execution_omp & ex, InQueue & input_queue, 
+                   farm_info<parallel_execution_omp, Transformer> & farm_obj, 
+                   MoreTransformers && ... more_transform_ops) 
+{
+  pipeline_impl(ex, input_queue, 
+      std::forward< farm_info<parallel_execution_omp,Transformer>>(farm_obj), 
+      std::forward<MoreTransformers>(more_transform_ops)...) ;
+}
 
+template <typename Transformer, typename InQueue,typename... MoreTransformers>
+void pipeline_impl(parallel_execution_omp & ex, InQueue & input_queue, 
+                   farm_info<parallel_execution_omp, Transformer> && farm_obj, 
+                   MoreTransformers && ... sgs ) 
+{
+  using namespace std;
+  using input_type = typename InQueue::value_type;
+  using input_value_type = typename input_type::first_type::value_type;
+  using result_type = typename result_of<Transformer(input_value_type)>::type;
+  using output_value_type = experimental::optional<result_type>;
+  using output_type = pair<output_value_type,long>;
+ 
+  mpmc_queue<output_type> output_queue{ex.queue_size, ex.lockfree};
+  atomic<int> nend{0}; // TODO: Better name?
+  for (int th=0; th<farm_obj.exectype.num_threads; th++) {
+    #pragma omp task shared(nend,output_queue,farm_obj,input_queue)
+    {
+      auto item = input_queue.pop();
+      while (item.first) {
+        auto out = output_value_type{farm_obj.task(*item.first)};
+        output_queue.push(make_pair(out,item.second));
+        item = input_queue.pop();
+      }
+      input_queue.push(item);
+      nend++;
+      if (nend==farm_obj.exectype.num_threads) {
+        output_queue.push(make_pair(output_value_type{}, -1));
+      }
+    }              
+  }
+  pipeline_impl(ex, output_queue, forward<MoreTransformers>(sgs) ... );
+  #pragma omp taskwait
+}
 
 //Intermediate stages
-template <typename Stage, typename Stream,typename ... Stages>
-void stages(parallel_execution_omp &p, Stream& st, Stage && se, Stages && ... sgs ) {
+template <typename Transformer, typename InQueue,typename ... MoreTransformers>
+void pipeline_impl(parallel_execution_omp & ex, InQueue & input_queue, 
+                   Transformer && transform_op, 
+                   MoreTransformers && ... more_transform_ops) 
+{
+  using namespace std;
+  using input_type = typename InQueue::value_type;
+  using input_value_type = typename input_type::first_type::value_type;
+  using result_type = typename result_of<Transformer(input_value_type)>::type;
+  using output_value_type = experimental::optional<result_type>;
+  using output_type = pair<output_value_type,long>;
+  mpmc_queue<output_type> output_queue{ex.queue_size, ex.lockfree};
 
-    //Create new queue
-    mpmc_queue<std::pair< std::experimental::optional <typename std::result_of<Stage(typename Stream::value_type::first_type::value_type)>::type >, long >> q(p.queue_size,p.lockfree);
-    //Start task
-    #pragma omp task shared( se, st, q )
-    {
-        typename Stream::value_type item;
-        item = st.pop( ); 
-        while( item.first ) {
-            auto out = std::experimental::optional <typename std::result_of<Stage(typename Stream::value_type::first_type::value_type)>::type > ( se(item.first.value()) );
-
-            q.push( std::make_pair(out, item.second) );
-            item = st.pop(  ) ;
-        }
-        q.push( std::make_pair(std::experimental::optional< typename std::result_of< Stage(typename Stream::value_type::first_type::value_type) > ::type>(),-1) ) ;
+  //Start task
+  #pragma omp task shared(transform_op, input_queue, output_queue)
+  {
+    auto item = input_queue.pop(); 
+    while (item.first) {
+      auto out = output_value_type{transform_op(*item.first)};
+      output_queue.push(make_pair(out, item.second));
+      item = input_queue.pop() ;
     }
-    //End task
-    //Create next stage
-    stages(p, q, std::forward<Stages>(sgs) ... );
-//    #pragma omp taskwait
+    output_queue.push(make_pair(output_value_type{}, -1));
+  }
+  //End task
+
+  pipeline_impl(ex, output_queue, 
+      forward<MoreTransformers>(more_transform_ops)...);
 }
 
-//First stage
-template <typename FuncIn, typename = typename std::result_of<FuncIn()>::type,
-          typename ...Stages,
-          requires_no_arguments<FuncIn> = 0>
-void pipeline(parallel_execution_omp &p, FuncIn && in, Stages && ... sts ) {
+/**
+\addtogroup pipeline_pattern
+@{
+*/
 
-    //Create first queue
-    mpmc_queue<std::pair< typename std::result_of<FuncIn()>::type, long>> q(p.queue_size,p.lockfree);
+/**
+\addtogroup pipeline_pattern_omp OpenMP parallel pipeline pattern
+\brief OpenMP parallel implementation of the \ref md_pipeline pattern
+@{
+*/
 
-    //Create stream generator stage
-    #pragma omp parallel
+/**
+\brief Invoke [pipeline pattern](@ref md_pipeline) on a data stream
+with OpenMP parallel execution.
+\tparam Generator Callable type for the stream generator.
+\tparam MoreTransformers Callable type for each transformation stage.
+\param ex Sequential execution policy object.
+\param generate_op Generator operation.
+\param trasnform_ops Transformation operations for each stage.
+\remark Generator shall be a zero argument callable type.
+*/
+template <typename Generator, typename ... MoreTransformers,
+          requires_no_arguments<Generator> = 0>
+void pipeline(parallel_execution_omp & ex, Generator && generate_op, 
+              MoreTransformers && ... more_transform_ops) 
+{
+  using namespace std;
+
+  using result_type = typename result_of<Generator()>::type;
+  mpmc_queue<pair<result_type,long>> output_queue{ex.queue_size,ex.lockfree};
+
+  #pragma omp parallel
+  {
+    #pragma omp single nowait
     {
-        #pragma omp single nowait
-        {
-            #pragma omp task shared(in,q)
-            {
-                long order = 0;
-                while( 1 ) {
-                    auto k = in();
-                    q.push( std::make_pair(k,order) ) ;
-                    order++;
-                    if( !k ) 
-                        break;
-                }
-            }
-            //Create next stage
-            stages(p, q, std::forward<Stages>(sts) ... );
-//            stages(p, q, sts ... );
-            #pragma omp taskwait
+      #pragma omp task shared(generate_op,output_queue)
+      {
+        long order = 0;
+        for (;;) {
+          auto item = generate_op();
+          output_queue.push(make_pair(item,order++)) ;
+          if (!item) break;
         }
+      }
+      pipeline_impl(ex, output_queue,
+          forward<MoreTransformers>(more_transform_ops)...);
+      #pragma omp taskwait
     }
+  }
 }
+
+/**
+@}
+@}
+*/
 
 }
 #endif
