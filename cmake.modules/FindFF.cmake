51a4820b92262612fd8b42c6b713e9c8dedb8b7a
fa-vio <fabio.trdn@gmail.com>
2018-01-26 11:02:29 +0100

    Changed FF git repo to today's version; restored original CMakeList.txt in unit_tests

--- cmake.modules/FindFF.cmake
+++ cmake.modules/FindFF.cmake
@@ -4,18 +4,15 @@ if (NOT EXISTS ${CMAKE_BINARY_DIR}/fastflow)
 
   ExternalProject_Add(fastflow
     GIT_REPOSITORY https://github.com/fastflow/fastflow.git
-    #GIT_TAG "master"
+    GIT_TAG "479817cf2dac93f732427693fb654f9f51b316f5"
     PREFIX fastflow
-    BUILD_IN_SOURCE
-    UPDATE_COMMAND ""
-    CMAKE_CACHE_ARGS
-      -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-      -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-      -DBUILD_TESTS:BOOL=OFF
-      -DBUILD_EXAMPLES:BOOL=OFF
+    BUILD_IN_SOURCE 0
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND     ""
+    INSTALL_COMMAND   ""
+    TEST_COMMAND      ""
   )
   set(FF_FOUND TRUE)
 endif()
 
-set(FFPATH ${CMAKE_BINARY_DIR}/fastflow)
+set(FFPATH ${CMAKE_BINARY_DIR}/fastflow/src)
