073a5abed0591d7b5ffad562abc7a67d2393e1fd
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2018-05-19 10:04:36 +0200

    Simulated range concept with meta-programming and detection idiom

--- include/range_mapping.h
+++ include/range_mapping.h
@@ -16,6 +16,8 @@
 #ifndef GRPPI_RANGE_MAPPING_H
 #define GRPPI_RANGE_MAPPING_H
 
+#include <experimental/type_traits>
+
 namespace grppi {
 
 template <typename C>
@@ -34,51 +36,79 @@ private:
   iterator first_, last_;
 };
 
-template <typename C>
+namespace meta {
+
+template <typename T, template <typename> class E>
+using has_member = std::experimental::is_detected<E,T>;
+
+template <typename T, typename R, template <typename> class E>
+using has_member_returning = std::experimental::is_detected_convertible<R,E,T>;
+
+template<typename ... Ts> 
+struct conjunction : std::true_type {};
+
+template<typename T> 
+struct conjunction<T> : T {};
+
+template<class T1, class... Ts>
+struct conjunction<T1, Ts...> : std::conditional_t<bool(T1::value), conjunction<Ts...>, T1> {};
+
+template <template <typename> class C, typename ... Ts>
+using requires = std::enable_if_t<meta::conjunction<C<Ts>...>::value,int>;
+
+}
+
+template <typename T>
+struct range_concept
+{
+  template <typename U>
+  using begin = decltype(std::declval<U>().begin());
+
+  template <typename U>
+  using has_begin = meta::has_member<U,begin>;
+
+  template <typename U>
+  using end = decltype(std::declval<U>().end());
+
+  template <typename U>
+  using has_end = meta::has_member<U,end>;
+
+  template <typename U>
+  using size = decltype(std::declval<const U>().size());
+
+  template <typename U>
+  using has_size = meta::has_member_returning<U,std::size_t,size>;
+
+  using requires = meta::conjunction<
+      has_begin<T>, 
+      has_end<T>,
+      has_size<T>>;
+
+  static constexpr bool value = requires::value;
+};
+
+template <typename T>
+using requires_range = meta::requires<range_concept,T>;
+
+template <typename ... Ts>
+using requires_ranges = meta::requires<range_concept,Ts...>;
+
+template <typename C,
+        requires_range<C> = 0>
 range_mapping<C> make_range(C & c) { return {c}; }
 
-template <typename ... Cs>
+template <typename ... Cs,
+        requires_ranges<Cs...> = 0>
 std::tuple<range_mapping<Cs>...> make_ranges(Cs & ... c) 
-{ 
+{
   return std::make_tuple(range_mapping<Cs>{c}...); 
 }
 
-
 template <typename R>
-struct is_range_impl
-{
-  static constexpr bool value = false;
-};
-
-template <typename C>
-struct is_range_impl<range_mapping<C>> {
-  static constexpr bool value = true;
-};
-
-template <typename R, typename ... Rs>
-struct are_ranges_impl
-{
-  static constexpr bool value = is_range_impl<R>::value && are_ranges_impl<Rs...>::value;
-};
-
-template <typename R>
-struct are_ranges_impl<R>
-{
-  static constexpr bool value = is_range_impl<R>::value;
-};
-
-
-template <typename R>
-constexpr bool is_range() { return is_range_impl<R>::value; }
+constexpr bool is_range() { return range_concept<R>::requires::value; }
 
 template <typename ... Rs>
-constexpr bool are_ranges() { return are_ranges_impl<Rs...>::value; } 
-
-template <typename R>
-using requires_range = std::enable_if_t<is_range<R>(), int>;
-
-template <typename ... Rs>
-using requires_ranges = std::enable_if_t<are_ranges<Rs...>(), int>;
+constexpr bool are_ranges() { return meta::conjunction<range_concept<Rs>...>::value; } 
 
 template <typename ... Rs, std::size_t ... I>
 std::tuple<typename Rs::iterator...> range_begin_impl(std::tuple<Rs...> rt, std::index_sequence<I...>)
@@ -86,13 +116,15 @@ std::tuple<typename Rs::iterator...> range_begin_impl(std::tuple<Rs...> rt, std:
   return std::make_tuple(std::get<I>(rt).begin()...);
 }
 
-template <typename ... Rs>
+template <typename ... Rs,
+        requires_ranges<Rs...> = 0>
 std::tuple<typename Rs::iterator...> range_begin(std::tuple<Rs...> rt) 
 {
   return range_begin_impl(rt, std::make_index_sequence<sizeof...(Rs)>{});
 }
 
-template <typename ... Rs>
+template <typename ... Rs,
+        requires_ranges<Rs...> = 0>
 auto range_size(std::tuple<Rs...> rt)
 {
   return std::get<0>(rt).size();
