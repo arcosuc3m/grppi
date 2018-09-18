cac8513e508ea23068775d691ad23c250faa88a3
David <drioastorga@gmail.com>
2018-09-18 10:55:52 +0200

    Added policy to google test

--- unit_tests/supported_executions.h
+++ unit_tests/supported_executions.h
@@ -20,6 +20,7 @@
 
 #include "seq/sequential_execution.h"
 #include "native/parallel_execution_native.h"
+#include "task/parallel_execution_task.h"
 #include "omp/parallel_execution_omp.h"
 #include "tbb/parallel_execution_tbb.h"
 #include "ff/parallel_execution_ff.h"
