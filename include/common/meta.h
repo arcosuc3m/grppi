34802e46a4e9723e430598e3ac8e1417db54f5e5
Miguel Sacristan <miguel_tete17@hotmail.com>
2019-01-11 01:45:40 +0100

    Allow bypassing experimental_detect macro for libc++ because the partial implementation present in 5.0 is sufficient

--- include/common/meta.h
+++ include/common/meta.h
@@ -18,8 +18,14 @@
 
 #if __has_include(<experimental/type_traits>)
 #  include <experimental/type_traits>
-#  if __cpp_lib_experimental_detect < 201505
-#    error "C++ detection idiom not supported. Upgrade your C++ compiler/standard library"
+#  ifdef _LIBCPP_VERSION
+#    if _LIBCPP_VERSION < 5000
+#      error "C++ detection idiom not supported. Upgrade your C++ standard library"
+#    endif
+#  else
+#    if __cpp_lib_experimental_detect < 201505
+#      error "C++ detection idiom not supported. Upgrade your C++ compiler/standard library"
+#    endif
 #  endif
 #else
 #  error "Experimental type traits not found. Upgrade your C++ compiler."
